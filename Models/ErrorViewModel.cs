baea9d2a142152fb5b9ed1d6e69de71dfcb4dd10
sarisoni <sariameli.sonilopez.gen23@cbtis105.edu.mx>
2025-05-26 09:51:20 -0600

    avance 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parcial3_Proyecto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
