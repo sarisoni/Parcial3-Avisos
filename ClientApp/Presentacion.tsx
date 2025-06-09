85477e2ed2d1d25e548088b514041596bd3a53ac
sarisoni <sariameli.sonilopez.gen23@cbtis105.edu.mx>
2025-06-09 10:12:06 -0600

    avance 1

---
+++ ClientApp/Presentacion.tsx
@@ -0,0 +1,16 @@
+const Presentacion = () => {
+// Manejo de datos
+
+//Vista
+return (
+    <>
+    <div className="display-4">Nombre del cbtis</div>
+    <div className="h1">Integrantes</div>
+    <div className="h2">Nombre del integrante 1</div>
+    <div className="h2">Nombre del integrante 2</div>
+      <div className="h1">Nombre del Proyecto</div>
+    </>
+)
+}
+
+export default Presentacion;
